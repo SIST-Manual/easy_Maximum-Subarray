9b0b5a2ef76efd401994b65a915e066c061b40f0
Q7 <q71998@gmail.com>
2018-07-25 00:33:04 +0800

    fix coding style

--- solve.py
+++ solve.py
@@ -1,13 +1,12 @@
-if __name__ == "__main__":
-    nums = list(map(int, input()[1:-1].split(",")))
-    if (len(nums) == 1):
-        print(nums[0])
-    cur_sum = None
-    prev_sum = nums[0]
-    cur_max = nums[0]
-    for i in range(1, len(nums)):
-        cur_sum = max(prev_sum + nums[i], nums[i])
-        if cur_max < cur_sum:
-            cur_max = cur_sum
-        prev_sum = cur_sum
-    print(cur_max)
+nums = list(map(int, input()[1:-1].split(",")))
+if (len(nums) == 1):
+    print(nums[0])
+cur_sum = None
+prev_sum = nums[0]
+cur_max = nums[0]
+for i in range(1, len(nums)):
+    cur_sum = max(prev_sum + nums[i], nums[i])
+    if cur_max < cur_sum:
+        cur_max = cur_sum
+    prev_sum = cur_sum
+print(cur_max)
